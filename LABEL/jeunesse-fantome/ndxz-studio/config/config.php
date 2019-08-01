51158f3952e1ff77e0589d9a83f45262aa237acd
Tristan Rivoallan <tristan.rivoallan-prestataire@laposte.fr>
2019-08-01 07:49:04 +0200

    Commit inaugural

---
+++ LABEL/jeunesse-fantome/ndxz-studio/config/config.php
@@ -0,0 +1,9 @@
+<?php  if (!defined('SITE')) exit('No direct script access allowed');
+
+$indx['db'] 		= 'spielzeugmuzak';
+$indx['user'] 		= 'spielzeugmuzak';
+$indx['pass'] 		= 'spielzeugmuzak';
+$indx['host'] 		= 'db';
+$indx['sql']		= 'mysql';
+
+?>
